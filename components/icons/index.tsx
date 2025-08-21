8e6d652121d0a335211ec534f9273c65f3521572
Alonso Toro <alonso.toro@ucp.edu.co>
2025-08-20 19:59:43 -0500

    Auto-commit: 1 modified file

--- components/icons/index.tsx
+++ components/icons/index.tsx
@@ -62,3 +62,9 @@ export const EyeIcon: React.FC<IconProps> = ({ className = "h-6 w-6" }) => (
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
     </svg>
 );
+
+export const HistoryIcon: React.FC<IconProps> = ({ className = "h-6 w-6" }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+);
